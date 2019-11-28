f7f26191a48ac8c8b1952181e9280bf1ee66ebe8
Raffael <raffael@pixeltastic.de>
2019-11-28 19:03:22 +0100

    Fixed Blazaco.Editor.Layout for current version

--- src/Blazaco/Editor/BlazacoJSInterop.cs
+++ src/Blazaco/Editor/BlazacoJSInterop.cs
@@ -3,13 +3,13 @@ using System.Threading.Tasks;
 
 namespace Blazaco.Editor
 {
-	public static class BlazacoJSInterop
+    public static class BlazacoJSInterop
     {
-		public static ValueTask<bool> InitializeEditor(IJSRuntime runtime, EditorModel editorModel)
-			=> runtime.InvokeAsync<bool>("Blazaco.Editor.InitializeEditor", new[] { editorModel });
+        public static ValueTask<bool> InitializeEditor(IJSRuntime runtime, EditorModel editorModel)
+            => runtime.InvokeAsync<bool>("Blazaco.Editor.InitializeEditor", new[] { editorModel });
 
-		public static ValueTask<string> GetValue(IJSRuntime runtime, string id)
-			=> runtime.InvokeAsync<string>("Blazaco.Editor.GetValue", new[] { id });
+        public static ValueTask<string> GetValue(IJSRuntime runtime, string id)
+            => runtime.InvokeAsync<string>("Blazaco.Editor.GetValue", new[] { id });
 
         public static ValueTask<bool> SetValue(IJSRuntime runtime, string id, string value)
             => runtime.InvokeAsync<bool>("Blazaco.Editor.SetValue", new[] { id, value });
@@ -17,7 +17,7 @@ namespace Blazaco.Editor
         public static ValueTask<bool> SetTheme(IJSRuntime runtime, string id, string theme)
             => runtime.InvokeAsync<bool>("Blazaco.Editor.SetTheme", new[] { id, theme });
 
-        public static Task<bool> Layout(IJSRuntime runtime, string id)
+        public static ValueTask<bool> Layout(IJSRuntime runtime, string id)
             => runtime.InvokeAsync<bool>("Blazaco.Editor.Layout", new[] { id });
     }
 }
